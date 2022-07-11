a450b17d4170e72dd42d9cf2a1b520bad53ba7c1
Teetwothefirst <dexterdavid835@gmail.com>
2022-07-10 18:03:55 -0700

    added but undecided

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
  * main - Entry point
  * My hello, print task
- * Return: return 0 after program execution
+ * Return: return 1 after program execution
  */
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,\n");
-	return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
